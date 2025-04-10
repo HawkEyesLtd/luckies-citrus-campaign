19f280fd605cdded2fc5408f82971290ae464589
MD Razikul Islam Joni <razikuljoni@gmail.com>
2025-04-10 16:18:50 +0600

    :refac: done the heading nav part

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,27 +3,19 @@
 import React from 'react';
 import 'react-multi-date-picker/styles/colors/yellow.css';
 // import logo from '../../assets/logo.png';
-import logo from '../../assets/ui/5x/Lucky Strike Logo@5x.png';
+import logo from '../../assets/ui/SVG/Logo.svg';
 import './nav.css';
 
 function NavBar() {
     return (
         <>
             <div className="nav-container">
+                <div className="background-overlay" />
                 <div className="nav-content">
-                    <img src={logo} alt="logo" width="180px" />
-                    <h1
-                        className="dashboard-heading"
-                        style={{
-                            margin: 0,
-                            lineHeight: '1',
-                            padding: '40px 0',
-                            marginLeft: '140px',
-                        }}
-                    >
-                        <span>Market Lenz</span>
-                        <span style={{ display: 'block' }}>DASHBOARD</span>
-                    </h1>
+                    <div className="content-wrapper">
+                        <img src={logo} alt="logo" width="200px" />
+                        <h1 className="dashboard-heading">S&D Dashboard</h1>
+                    </div>
                 </div>
             </div>
             <div style={{ width: '100%', height: '8px', background: '#020202' }} />
